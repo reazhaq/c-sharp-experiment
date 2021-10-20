2ed9709afa2d801474f13741620e4a7e04d9121f
reaz haq <reaz.haq@outlook.com>
2021-10-20 15:50:58 -0500

    readme doc c# 7 - tuples

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# c-sharp-experiment
+# Timeline (very high level)
+
+* 1 (~2002-03): first release
+* 2 (~2005-06): Generics, Nullable Types, Anonymous methods, etc.
+* 3 (~2007-08): Lambda, Extension Methods, Anonymous types, Expression tree, Auto-Property, etc.
+* 4 (~2010): Covariance-Contravariance, Optional Paramters, Dynamic Binding, COM interop, Task, etc.
+* 5 (~2011-2012) async-await, caller information
+* 6 (~2014): Null conditional Op, Auto Property initializer, Name Of, Expression body, string interpolation, etc.
+* [7._ (2017-2018)](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-7): value tuple, deconstructor (not destructor/finalizer), out variable, pattern matching, local function, ref. return, expression body (enhancement), value task, etc.
+  * async main, `default` literal, some tuple enhancements, some patter matching improvements, ref local in returns, in parameter (more like readonly), generic constraint (enhancement)
+* [8 (2019)](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8): readonly members, default interface method, pattern matching (enhancement), using declaration, static local function, disposable ref structure, nullable ref types, async stream, async disposable, indices and ranges, null-coalescing assignment, unmanaged constructed types, stackalloc in nested expression, enhancement of interpolated verbatim strings
+* [9 (2020)](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-9): records, init-only setters, pattern matching (enhancement), native sized int, function pointers, suppress emitting localsinit flag, target-type new expression, covariant return type, get-enumerator extension, lambda discard parameters, attributes on local function, module initializers, new feature for partial methods
+* [10 (2021)](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-10): record structs, improvements of structure types, global using directive, file-scoped namespace, extended property patterns, allow constant interpolated string, seal to-string override for record type, both assignment and declaration in same desconstruction, async-method-bulider attribute
+ 
+for details - look at [The History of C#](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-version-history)
---
+++ c_sharp_7/Tuples.cs
@@ -0,0 +1,137 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace c_sharp_7
+{
+    /// <summary>
+    /// old style tuple was ref type - new one is value type
+    /// old one had to be used as item1, item2, etc.
+    /// new one - items can have names
+    /// </summary>
+    public class Tuples
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Tuples(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Old_styles_tuples_are_clunky()
+        {
+            var person = new Tuple<string, int>("john doe", 37);
+
+            // but they are accessed using Item1, Item2, ... not so user friendly
+            Assert.Equal("john doe", person.Item1);
+            Assert.Equal(37, person.Item2);
+            _testOutputHelper.WriteLine($"{person.GetType()}");
+            Assert.Equal(typeof(Tuple<string, int>), person.GetType());
+        }
+
+        [Fact]
+        public void ValueTuple_with_named_properties()
+        {
+            // tuple can be declared like this
+            (string Name, int Age) person = ("john doe", 37);
+
+            Assert.Equal("john doe", person.Name);
+            Assert.Equal(37, person.Age);
+            _testOutputHelper.WriteLine($"{person.GetType()}");
+            Assert.Equal(typeof(ValueTuple<string, int>), person.GetType());
+        }
+
+        [Fact]
+        public void ValueTuple_with_named_properties2()
+        {
+            // tuple can be declared like this
+            var person = (Name: "john doe", Age: 37);
+
+            Assert.Equal("john doe", person.Name);
+            Assert.Equal(37, person.Age);
+            _testOutputHelper.WriteLine($"{person.GetType()}");
+            Assert.Equal(typeof(ValueTuple<string, int>), person.GetType());
+        }
+
+        [Fact]
+        public void ValueTuple_with_named_properties3()
+        {
+            // tuple can be declared like this - old style
+            var person = ("john doe", 37);
+
+            // but they are accessed using Item1, Item2, ... not so user friendly
+            Assert.Equal("john doe", person.Item1);
+            Assert.Equal(37, person.Item2);
+            _testOutputHelper.WriteLine($"{person.GetType()}");
+            Assert.Equal(typeof(ValueTuple<string, int>), person.GetType());
+        }
+
+        [Fact]
+        public void Function_return_ValueTuple_old_style()
+        {
+            var person = GetPerson();
+
+            // but they are accessed using Item1, Item2, ... not so user friendly
+            Assert.Equal("john doe", person.Item1);
+            Assert.Equal(37, person.Item2);
+            _testOutputHelper.WriteLine($"{person.GetType()}");
+            Assert.Equal(typeof(ValueTuple<string, int>), person.GetType());
+        }
+
+        // returns old style tuple but value type
+        (string, int) GetPerson()
+        {
+            return ("john doe", 37);
+        }
+
+        [Fact]
+        public void Function_return_ValueTuple_with_names()
+        {
+            var person = GetPerson2();
+
+            Assert.Equal("john doe", person.Name);
+            Assert.Equal(37, person.Age);
+            _testOutputHelper.WriteLine($"{person.GetType()}");
+            Assert.Equal(typeof(ValueTuple<string, int>), person.GetType());
+        }
+
+        (string Name, int Age) GetPerson2()
+        {
+            return ("john doe", 37);
+        }
+
+        [Fact]
+        public void Nice_way_to_return_anonymous_type_looking_object_but_value_type()
+        {
+            var aObject = GetCustomerRecord();
+
+            Assert.Equal("first", aObject.firstName);
+            Assert.Equal("last", aObject.lastName);
+            Assert.Equal(24, aObject.age);
+            _testOutputHelper.WriteLine($"{aObject.GetType()}{Environment.NewLine}");
+            Assert.Equal(typeof(ValueTuple<string, string, int>), aObject.GetType());
+
+            var aObject2 = GetCustomerRecord2();
+            _testOutputHelper.WriteLine($"aObject2.GetType(): {aObject2.GetType()}{Environment.NewLine}");
+            // aObject2.  <- this doesn't work
+        }
+
+        // nice way to return anonymous type looking object; but a value type
+        (string firstName, string lastName, int age) GetCustomerRecord()
+        {
+            // this anonObject can't be returned
+            var anonObject = new {fn = "first", ln = "last", a = 24};
+            _testOutputHelper.WriteLine($"anonObject.GetType(): {anonObject.GetType()}{Environment.NewLine}");
+
+            // but data can be returned as value tuple
+            return (anonObject.fn, anonObject.ln, anonObject.a);
+        }
+
+        // side note - you can return an object but past that you are stuck with reflection
+        object GetCustomerRecord2()
+        {
+            return new { fn = "first", ln = "last", a = 24 };
+        }
+    }
+}

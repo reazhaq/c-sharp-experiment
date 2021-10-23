253afa208027f31e93bc8d19c8b90af5ed8aa8c0
reaz haq <reaz.haq@outlook.com>
2021-10-23 09:37:03 -0500

    c-7: ref-return and expression body

---
+++ c_sharp_7/E6RefReturn.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace c_sharp_7
+{
+    public class E6RefReturn
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+        private int[] someNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        public E6RefReturn(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void ToUpdate_an_element_of_an_array_find_index_first()
+        {
+            var valueToLookFor = 3;
+
+            // before: get the index and then 
+            var indexOf3 = FindElementWithValueAndReturnIndex(valueToLookFor, someNumbers);
+            Assert.Equal(2, indexOf3);
+            Assert.Equal(valueToLookFor, someNumbers[indexOf3]);
+
+            // change the value
+            someNumbers[indexOf3] = 99;
+            // now search for the same value throws exception;
+            var exception = Record.Exception(() => FindElementWithValueAndReturnIndex(valueToLookFor, someNumbers));
+            Assert.Equal($"{valueToLookFor} not found", exception.Message);
+        }
+
+        private int FindElementWithValueAndReturnIndex(int numberToFind, int[] sourceWhereToLook)
+        {
+            for(int i = 0; i < sourceWhereToLook.Length; i++)
+                if(numberToFind == sourceWhereToLook[i])
+                    return i;
+
+            throw new Exception($"{numberToFind} not found");
+        }
+
+        [Fact]
+        public void ToUpdate_an_element_of_an_array_use_ref_return_that_returns_a_direct_reference_to_the_element()
+        {
+            var valueToLookFor = 4;
+
+            var indexOf4 = FindElementWithValueAndReturnIndex(valueToLookFor, someNumbers);
+            ref var elementWithValue4 = ref FindElementWithValueAndReturnRef(valueToLookFor, someNumbers);
+
+            // i can access the element by index
+            Assert.Equal(valueToLookFor, someNumbers[indexOf4]);
+            // i also have direct access
+            Assert.Equal(valueToLookFor, elementWithValue4);
+            // update the element value directly - no need to access by index
+            elementWithValue4 = 99;
+            // validate correct element got updated
+            Assert.Equal(99, someNumbers[indexOf4]);
+            // now search for the same value throws exception;
+            var exception = Record.Exception(() => FindElementWithValueAndReturnIndex(valueToLookFor, someNumbers));
+            Assert.Equal($"{valueToLookFor} not found", exception.Message);
+        }
+
+        // note "ref" in return signature - and ref in the return statement
+        private ref int FindElementWithValueAndReturnRef(int numberToFind, int[] sourceWhereToLook)
+        {
+            for (int i = 0; i < sourceWhereToLook.Length; i++)
+                if (numberToFind == sourceWhereToLook[i])
+                    return ref sourceWhereToLook[i];
+
+            throw new Exception($"{numberToFind} not found");
+        }
+    }
+}
---
+++ c_sharp_7/E7ExpressionBody.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace c_sharp_7
+{
+    /// <summary>
+    /// expression body => means one liner - get rid of those curly braces
+    /// </summary>
+    public class E7ExpressionBody
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public E7ExpressionBody(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        private class SomeClass : IDisposable
+        {
+            private readonly ITestOutputHelper testOutputHelper;
+
+            public SomeClass(string name, ITestOutputHelper testOutputHelper)
+            {
+                MyName = name;
+                this.testOutputHelper = testOutputHelper;
+            }
+
+            // expression body call - very simple one line
+            ~SomeClass() => InternalDispose(false);
+
+            // property backed by private field
+            private string myName;
+            public string MyName
+            {
+                // expression body
+                get => myName;
+                // expression body with some validation
+                set => myName = value ?? throw new ArgumentNullException(nameof(value));
+            }
+
+            // another one-liner
+            public void Dispose() => InternalDispose(true);
+
+            protected void InternalDispose(bool isDisposing)
+            {
+                if (isDisposing)
+                {
+                    testOutputHelper.WriteLine($"IDisposable.Dispose called");
+                    GC.SuppressFinalize(this);
+                }
+                else
+                {
+                    testOutputHelper.WriteLine($"this call came from finalizer");
+                }
+            }
+        }
+
+        [Fact]
+        public void Dispose_calls_internal_using_oneLine_expressionBody()
+        {
+            using (var someClass = new SomeClass("john doe", testOutputHelper))
+            {
+                testOutputHelper.WriteLine($"my name is: {someClass.MyName}");
+                testOutputHelper.WriteLine($"variable someclass going out of scope - dispose shall be called - here");
+            }
+        }
+
+        [Fact]
+        public void Try_to_set_name_to_NULL_throws_exception()
+        {
+            var someClass = new SomeClass("john doe", testOutputHelper);
+            var exception = Record.Exception(() => someClass.MyName = null);
+
+            testOutputHelper.WriteLine($"exception.message: {exception.Message}");
+            Assert.NotNull(exception);
+        }
+    }
+}

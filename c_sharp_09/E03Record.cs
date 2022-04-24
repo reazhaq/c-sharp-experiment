164b03b8a5379fa5efbece05716da098ee54bde5
reaz haq <reaz.haq@outlook.com>
2022-04-24 18:53:51 -0500

    equality with record is confusing....

--- c_sharp_09/E03Record.cs
+++ c_sharp_09/E03Record.cs
@@ -2,87 +2,88 @@
 using System.Collections.Generic;
 using Xunit;
 
-namespace c_sharp_09;
-
-/// <summary>
-/// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-9.0/records
-/// </summary>
-
-public class E03Record
+namespace c_sharp_09
 {
-    public sealed record PersonWithHobbies
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-9.0/records
+    /// </summary>
+
+    public class E03Record
     {
-        public string FirstName { get; init; }
-        public string LastName { get; init; }
-        public IReadOnlyCollection<string> Hobbies { get; init; }
-
-        public PersonWithHobbies(string firstName, string lastName, List<string> hobbies)
+        public sealed record PersonWithHobbies
         {
-            FirstName = firstName;
-            LastName = lastName;
+            public string FirstName { get; init; }
+            public string LastName { get; init; }
+            public IReadOnlyCollection<string> Hobbies { get; init; }
 
-            if(hobbies == null)
-                hobbies = new List<string>();
-            Hobbies = hobbies.AsReadOnly();
+            public PersonWithHobbies(string firstName, string lastName, List<string> hobbies)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+
+                if (hobbies == null)
+                    hobbies = new List<string>();
+                Hobbies = hobbies.AsReadOnly();
+            }
+
+            public bool Equals(PersonWithHobbies? p)
+            {
+                if (p == null)
+                    return false;
+
+                return FirstName.Equals(p.FirstName) && LastName.Equals(p.LastName) && Hobbies.Count.Equals(p.Hobbies.Count);
+            }
+
+            public override int GetHashCode() => HashCode.Combine(FirstName, LastName, Hobbies.Count);
         }
 
-        public bool Equals(PersonWithHobbies? p)
+        [Fact]
+        public void HobbiesAreReadOnly()
         {
-            if(p == null)
-                return false;
+            var p1 = new PersonWithHobbies("wonder", "woman", new List<string> { "hiking", "biking" });
 
-            return FirstName.Equals(p.FirstName) && LastName.Equals(p.LastName) && Hobbies.Count.Equals(p.Hobbies.Count);
+            //// no allowed since it is readonly
+            //p1.Hobbies.Add...
+            Assert.Equal(2, p1.Hobbies.Count);
+            Assert.IsAssignableFrom<IReadOnlyCollection<string>>(p1.Hobbies);
         }
 
-        public override int GetHashCode() => HashCode.Combine(FirstName, LastName, Hobbies.Count);
-    }
+        [Fact]
+        public void RecordWithRefTypeTest1()
+        {
+            var p1 = new PersonWithHobbies("captain", "america", new List<string> { "hiking", "biking" });
+            var p2 = new PersonWithHobbies("captain", "america", new List<string> { "hiking", "fishing" });
 
-    [Fact]
-    public void HobbiesAreReadOnly()
-    {
-        var p1 = new PersonWithHobbies("wonder", "woman", new List<string> { "hiking", "biking" });
+            var p1EqualsP2 = p1.Equals(p2);
 
-        //// no allowed since it is readonly
-        //p1.Hobbies.Add...
-        Assert.Equal(2, p1.Hobbies.Count);
-        Assert.IsAssignableFrom<IReadOnlyCollection<string>>(p1.Hobbies);
-    }
+            // they are equal - cause we have Equal override
+            Assert.True(p1EqualsP2);
+            Assert.False(ReferenceEquals(p1, p2));
+        }
 
-    [Fact]
-    public void RecordWithRefTypeTest1()
-    {
-        var p1 = new PersonWithHobbies("captain", "america", new List<string> { "hiking", "biking" });
-        var p2 = new PersonWithHobbies("captain", "america", new List<string> { "hiking", "fishing" });
+        [Fact]
+        public void Got_iEquatable_out_of_the_box()
+        {
+            var p1 = new PersonWithHobbies("bruce", "banner", new List<string> { "hiking", "fishing" });
 
-        var p1EqualsP2 = p1.Equals(p2);
+            var equatableP1 = p1 as IEquatable<PersonWithHobbies>;
 
-        // they are equal - cause we have Equal override
-        Assert.True(p1EqualsP2);
-        Assert.False(ReferenceEquals(p1, p2));
-    }
+            // record automatically implements IEquatable interface
+            Assert.NotNull(equatableP1);
+            Assert.IsAssignableFrom<IEquatable<PersonWithHobbies>>(p1);
+        }
 
-    [Fact]
-    public void Got_iEquatable_out_of_the_box()
-    {
-        var p1 = new PersonWithHobbies("bruce", "banner", new List<string> { "hiking", "fishing" });
+        [Fact]
+        public void Got_equal_equal_out_of_the_box()
+        {
+            var p1 = new PersonWithHobbies("bruce", "banner", new List<string> { "hunting", "fishing" });
+            var p2 = new PersonWithHobbies("bruce", "banner", new List<string> { "hunting", "fishing" });
 
-        var equatableP1 = p1 as IEquatable<PersonWithHobbies>;
+            var p1EqualsP2 = p1 == p2;
 
-        // record automatically implements IEquatable interface
-        Assert.NotNull(equatableP1);
-        Assert.IsAssignableFrom<IEquatable<PersonWithHobbies>>(p1);
-    }
-
-    [Fact]
-    public void Got_equal_equal_out_of_the_box()
-    {
-        var p1 = new PersonWithHobbies("bruce", "banner", new List<string> { "hunting", "fishing" });
-        var p2 = new PersonWithHobbies("bruce", "banner", new List<string> { "hunting", "fishing" });
-
-        var p1EqualsP2 = p1 == p2;
-
-        Assert.True(p1EqualsP2);
-        // p1 and p2 are two different object in memory
-        Assert.False(ReferenceEquals(p1, p2));
+            Assert.True(p1EqualsP2);
+            // p1 and p2 are two different object in memory
+            Assert.False(ReferenceEquals(p1, p2));
+        }
     }
 }
---
+++ c_sharp_09/E04Record.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Xunit;
+
+namespace c_sharp_09
+{
+    public class E04Record
+    {
+        public record Person(string firstName, string lastName);
+
+        public record Student(string firstName, string lastName, int id) : Person(firstName, lastName);
+
+        [Fact]
+        public void EqualsAreConfusing()
+        {
+            var p1 = new Person("doctor", "strange");
+            var s1 = new Student("doctor", "strange", 1);
+
+            var p1EqualsS1 = p1.Equals(s1);
+            Assert.False(p1EqualsS1);
+
+            var s1EqualsP1 = s1.Equals(p1);
+            Assert.False(s1EqualsP1);
+
+            // this one fails too - that's strange
+            // should we not be able to cast to base object
+            // and expect them to be equal
+            var ps = (Person)s1;
+            Assert.False(ps.Equals(p1));
+        }
+
+        public class PersonClass : IEquatable<PersonClass>
+        {
+            public string FirstName { get; init; }
+            public string LastName { get; init; }
+
+            public PersonClass(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+            public virtual bool Equals(PersonClass? other)
+            {
+                if (other == null)
+                    return false;
+
+                return FirstName.Equals(other.FirstName) && LastName.Equals(other.LastName);
+            }
+        }
+
+        public class StudentClass : PersonClass, IEquatable<StudentClass>
+        {
+            public int Id { get; init; }
+
+            public StudentClass(string firstName, string lastName, int id) : base(firstName, lastName)
+            {
+                Id = id;
+            }
+
+            public bool Equals(StudentClass? other)
+            {
+                if (other == null)
+                    return false;
+
+                return Id == other.Id && base.Equals(other);
+            }
+        }
+
+        [Fact]
+        public void EqualsAre_not_Confusing()
+        {
+            var p1 = new PersonClass("doctor", "strange");
+            var s1 = new StudentClass("doctor", "strange", 1);
+
+            var p1EqualsS1 = p1.Equals(s1);
+            Assert.True(p1EqualsS1);
+
+            var s1EqualsP1 = s1.Equals(p1);
+            Assert.True(s1EqualsP1);
+
+            var ps = (PersonClass)s1;
+            Assert.True(ps.Equals(p1));
+        }
+
+    }
+}

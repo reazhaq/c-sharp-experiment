7a1582b2ab5bc9052a9c40f1d57f65307e805b2f
reaz haq <reaz.haq@outlook.com>
2022-04-27 05:58:55 -0500

    record-stack-overflow

--- c_sharp_09/E07Record_stack_overflow.cs
+++ c_sharp_09/E07Record_stack_overflow.cs
@@ -17,31 +17,73 @@ namespace c_sharp_09
             public Twin? Other { get; set; }
         }
 
-        [Fact]
+        // this is stack overflow on a to-string is by design
+        // https://github.com/dotnet/roslyn/issues/49396
+        [Fact(Skip = "stack-overflow")]
         public void ToString_causes_stack_overflow()
         {
-            var tA = new Twin("A");
-            var tB = new Twin("B"); 
-            tA.Other = tB;
-            tB.Other = tA;
+            var wanda = new Twin("Wanda");
+            var pietro = new Twin("Pietro"); 
+            wanda.Other = pietro;
+            pietro.Other = wanda;
 
-            testOutputHelper.WriteLine($"{tA}");
+            testOutputHelper.WriteLine($"{wanda}");
         }
 
         [Fact]
         public void Spinning_in_a_circle()
         {
-            var tA = new Twin("A");
-            var tB = new Twin("B");
-            tA.Other = tB;
-            tB.Other = tA;
+            var wanda = new Twin("Wanda");
+            var pietro = new Twin("Pietro");
+            wanda.Other = pietro;
+            pietro.Other = wanda;
 
-            Assert.Equal(tA, tB.Other);
+            Assert.Equal(wanda, pietro.Other);
+            // this was an issue with early release but it is fixed now
             // test runs fine - but trying to debug fails
             // set a breakpoint and during debug - try to hover
-            // over tA
-            // this was an issue with early release but it is fixed now
-            Assert.Equal(tA, tA.Other.Other);
+            // over wanda
+            Assert.Equal(wanda, wanda.Other.Other);
+        }
+
+        [Fact]
+        public void Twin_makes_new_records()
+        {
+            var wanda = new Twin("Wanda");
+            var pietro = new Twin("Pietro");
+
+            // what's going on here...
+            // does these assignments create new objects?
+            wanda.Other = pietro;
+            pietro.Other = wanda;
+        }
+
+        /// <summary>
+        /// sharplab generated code - this causes stack overflow
+        /// go to https://sharplab.io/
+        /// enter the following code
+        /// var wanda = new Twin("Wanda");
+        /// var pietro = new Twin(name: "Pietro");
+        /// 
+        /// wanda.Other = pietro;
+        /// pietro.Other = pietro;
+        /// 
+        /// System.Console.Write($"{wanda == wanda.Other.Other}");
+        /// 
+        /// public record Twin(string name)
+        /// {
+        ///     public Twin? Other { get; set; }
+        /// }
+        /// 
+        /// now look at the generated main function
+        /// try to run it - it fails
+        /// </summary>
+        [Fact(Skip = "sharplab generated code - stack overflow")]
+        public void Twin_makes_new_records2()
+        {
+            Twin twin = new Twin("Wanda");
+            Twin twin3 = (twin.Other = new Twin("Pietro"));
+            twin3.Other = twin3;
         }
     }
 }
